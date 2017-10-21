084051cb6d11e8b05b680e496a379c6f5ec2408e
Yura <yura.shimon98@gmail.com>
2017-10-21 18:50:21 +0300

    posts

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :posts
+  validates :title, :body, presence: true
+  validates :title, length: 1..10
+
+end
